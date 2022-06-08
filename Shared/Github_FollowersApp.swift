4d70cd42f872b8b29badc96088bdbefb30ab52ab
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-06-08 11:35:03 +0800

    Initial Commit

---
+++ Shared/Github_FollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_FollowersApp.swift
+//  Shared
+//
+//  Created by Jayvee Ballesteros on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct Github_FollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
